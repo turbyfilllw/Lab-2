196a97203410dd8b5f136d6a610365a33c0bbaa4
Logan Turbyfill <TURBYFILLLW@ETSU.EDU>
2022-09-01 18:44:42 -0400

    Part 1 finished

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//problem 1 
+Sum();
+Clear();
+static void Sum()
+{
+    int num1, num2;
+    Console.Write("Enter the first number: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter the second number: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+}
+static void Clear()
+{
+    Console.WriteLine("Press a key to move on...");
+    Console.ReadLine();
+    Console.Clear();
+}
